37db0dfd7a061d516f96f1c93cc3e1db1f1a4a7f
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-22 15:01:30 +0330

    get data from api

---
+++ js/game.js
@@ -0,0 +1,18 @@
+const loader=document.getElementById("loader")
+const container=document.getElementById("container")
+const URL="https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple"
+let formattedData=null;
+const fetchData=async()=>{
+    const response=await fetch(URL)
+    const json=await  response.json()
+    console.log(json)
+    formattedData=json
+    start()
+}
+
+const start=()=>{
+      loader.style.display="none"
+      container.style.display="block"
+}
+
+window.addEventListener("load",fetchData)
